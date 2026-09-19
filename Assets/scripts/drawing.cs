ebb2d88904a75c0a1dfcd3cad1583cae6a5ff371
cofBy <foxmagic24@gmail.com>
2026-09-19 08:56:09 +0300

    making a mesh
    After a bit of confusion I finally figured out how to read the texture data and make a mesh out of it. It's not marched squares yet it's just squares. it uses gpu to read the data

--- Assets/scripts/drawing.cs
+++ Assets/scripts/drawing.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class drawing : MonoBehaviour
 {
     [Header("drawing")]
-    public float radius;
+    public int radius;
 
     [Header("rendering texture")]
     public Mesh quad;
@@ -16,6 +16,9 @@ public class drawing : MonoBehaviour
     public ComputeShader computeDraw;
     Vector2 groupSize;
 
+    [Header("marching squares")]
+    public marchingSquares marcher;
+
     private void Awake()
     {
         uint threadX;
@@ -23,12 +26,14 @@ public class drawing : MonoBehaviour
         computeDraw.GetKernelThreadGroupSizes(0, out threadX, out threadY, out _);
         groupSize = new Vector2((float)pixelSize.x / threadX, (float)pixelSize.y / threadY);
 
-        drawingTexture = new RenderTexture(pixelSize.x, pixelSize.y, 0,RenderTextureFormat.R16);
+        drawingTexture = new RenderTexture(pixelSize.x, pixelSize.y, 0, RenderTextureFormat.R8);
         drawingTexture.filterMode = FilterMode.Point;
         drawingTexture.enableRandomWrite = true;
 
         computeDraw.SetTexture(0, "Result", drawingTexture);
         fullscreenMat.SetTexture("_data", drawingTexture);
+
+        marcher.dataTexture = drawingTexture;
     }
 
     private void Update()
@@ -37,8 +42,11 @@ public class drawing : MonoBehaviour
         computeDraw.SetInts("res",      new int[2] { Screen.width, Screen.height });
         computeDraw.SetInts("pixelRes", new int[2] { pixelSize.x, pixelSize.y});
         computeDraw.SetBool("isDrawing", Input.GetMouseButton(0));
-        computeDraw.SetFloat("radius", radius);
-        computeDraw.Dispatch(0, (int)groupSize.x, (int)groupSize.y, 1);
+        computeDraw.SetInt("radius", radius);
+        for (int i = 0; i < 10; i++)
+        {
+            computeDraw.Dispatch(0, (int)groupSize.x, (int)groupSize.y, 1);
+        }
 
         Graphics.DrawMesh(quad, Matrix4x4.Scale(2f * new Vector3(((float)Screen.width / Screen.height) * Camera.main.orthographicSize, Camera.main.orthographicSize)), fullscreenMat, 0);
     }
--- Assets/scripts/marchingSquares.cs
+++ Assets/scripts/marchingSquares.cs
@@ -1,7 +1,82 @@
+using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class marchingSquares : MonoBehaviour
 {
-    [Header("getting textures")]
-    public drawing textureProvider;
+    [Header("getting the texture")]
+    public RenderTexture dataTexture;
+
+    [Header("marching squares")]
+    public MeshFilter filter;
+    Mesh marchedSquares;
+    bool readbackInProgress = false;
+
+    private void Awake()
+    {
+        marchedSquares = new Mesh();
+        marchedSquares.name = "marched squares";
+    }
+
+    void Update()
+    {
+        if (!readbackInProgress)
+        {
+            readbackInProgress = true;
+            AsyncGPUReadback.Request(dataTexture, 0, TextureFormat.R8, OnCompleteReadback);
+        }
+    }
+
+    void OnCompleteReadback(AsyncGPUReadbackRequest request)
+    {
+        readbackInProgress = false;
+        if (request.hasError)
+        {
+            Debug.LogWarning("GPU readback error detected.");
+            return;
+        }
+        if (marchedSquares == null) return;
+
+        NativeArray<byte> data = request.GetData<byte>();
+
+        List<Vector3> verts = new List<Vector3>(0);
+        List<int> tris = new List<int>(0);
+        for (int y = 0; y < request.width; y++)
+        {
+            for (int x = 0; x < request.height; x++)
+            {
+                byte pixelColor = data[y * request.width + x];
+
+                if (pixelColor == 0) continue;
+
+                float sizeY = Camera.main.orthographicSize;
+                float sizeX = sizeY * (Screen.width / Screen.height);
+                float realX = ((float)x / request.width - 0.5f) * sizeX * 2f;
+                float realY = ((float)y / request.height - 0.5f) * sizeY * 2f;
+
+                float difX = 1f / request.width * sizeX * 2f;
+                float difY = 1f / request.height * sizeY * 2f;
+                int baseIndex = verts.Count;
+                verts.Add(new Vector3(realX     , realY     ));
+                verts.Add(new Vector3(realX+difX, realY     ));
+                verts.Add(new Vector3(realX+difX, realY+difY));
+                verts.Add(new Vector3(realX     , realY+difY));
+
+                tris.Add(baseIndex + 0);
+                tris.Add(baseIndex + 1);
+                tris.Add(baseIndex + 2);
+                tris.Add(baseIndex + 0);
+                tris.Add(baseIndex + 2);
+                tris.Add(baseIndex + 3);
+            }
+        }
+        marchedSquares.vertices = verts.ToArray();
+        marchedSquares.triangles = tris.ToArray();
+        filter.mesh = marchedSquares;
+    }
+    private void OnDestroy()
+    {
+        Destroy(marchedSquares);
+    }
 }
