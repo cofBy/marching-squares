3af28fbeb797220c8cc575bb965c74f90676ce4b
cofBy <foxmagic24@gmail.com>
2026-09-22 21:26:54 +0300

    made a build

--- Assets/scripts/drawing.cs
+++ Assets/scripts/drawing.cs
@@ -7,7 +7,6 @@ public class drawing : MonoBehaviour
     public float strength;
 
     [Header("rendering texture")]
-    public bool renderDrawing;
     public Mesh quad;
     public Vector2Int pixelSize;
 
@@ -23,26 +22,26 @@ public class drawing : MonoBehaviour
 
     private void Awake()
     {
+        drawingTexture = new RenderTexture(pixelSize.x, pixelSize.y, 0, RenderTextureFormat.R8);
+        drawingTexture.filterMode = FilterMode.Bilinear;
+        drawingTexture.enableRandomWrite = true;
+        drawingTexture.Create();
+
         uint threadX;
         uint threadY;
         computeDraw.GetKernelThreadGroupSizes(0, out threadX, out threadY, out _);
         groupSize = new Vector2((float)pixelSize.x / threadX, (float)pixelSize.y / threadY);
-
-        drawingTexture = new RenderTexture(pixelSize.x, pixelSize.y, 0, RenderTextureFormat.R8);
-        drawingTexture.filterMode = FilterMode.Bilinear;
-        drawingTexture.enableRandomWrite = true;
-
         computeDraw.SetTexture(0, "Result", drawingTexture);
-        fullscreenMat.SetTexture("_data", drawingTexture);
 
+        fullscreenMat.SetTexture("_data", drawingTexture);
         marcher.dataTexture = drawingTexture;
     }
 
     private void Update()
     {
-        computeDraw.SetInts("mousePos", new int[2] { (int)Input.mousePosition.x, (int)Input.mousePosition.y});
-        computeDraw.SetInts("res",      new int[2] { Screen.width, Screen.height });
-        computeDraw.SetInts("pixelRes", new int[2] { pixelSize.x, pixelSize.y});
+        computeDraw.SetInts("mousePos", new int[2] { (int)Input.mousePosition.x, (int)Input.mousePosition.y });
+        computeDraw.SetInts("res", new int[2] { Screen.width, Screen.height });
+        computeDraw.SetInts("pixelRes", new int[2] { pixelSize.x, pixelSize.y });
         computeDraw.SetInt("isDrawing", Input.GetMouseButton(0) ? 1 : Input.GetMouseButton(1) ? -1 : 0);
         computeDraw.SetFloat("radius", radius);
         computeDraw.SetFloat("strength", strength);
@@ -50,8 +49,6 @@ public class drawing : MonoBehaviour
         {
             computeDraw.Dispatch(0, (int)groupSize.x, (int)groupSize.y, 1);
         }
-
-        if (renderDrawing)
-            Graphics.DrawMesh(quad, Matrix4x4.Scale(2f * new Vector3(((float)Screen.width / Screen.height) * Camera.main.orthographicSize, Camera.main.orthographicSize)), fullscreenMat, 0);
+        Graphics.DrawMesh(quad, Matrix4x4.Scale(2f * new Vector3(((float)Screen.width / Screen.height) * Camera.main.orthographicSize, Camera.main.orthographicSize)), fullscreenMat, 0);
     }
 }
